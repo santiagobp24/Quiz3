10d58908a9f7d1583f731df2a91ac3f5ec01b481
santiagobp24 <santiago83iti@gmail.com>
2022-03-17 23:15:25 -0500

    primerC

---
+++ src/main/java/Ejemplos004/Central/A.java
@@ -0,0 +1,10 @@
+package Ejemplos004.Central;
+
+public class A {
+	public static void main (String[] args) 
+	{
+		int a = 9;
+		System.out.println(a+a);
+	}
+
+}
---
+++ src/main/java/Ejemplos004/Central/App.java
@@ -0,0 +1,13 @@
+package Ejemplos004.Central;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
